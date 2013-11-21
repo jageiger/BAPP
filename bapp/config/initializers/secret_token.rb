f0815ace2c293b3de192344253ed930c891dd4d1
Davenn <dmannix@asu.edu>
2013-11-21 11:29:11 -0700

    Initial Setup

---
+++ bapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bapp::Application.config.secret_key_base = '16674e242a3f8f6e5f2a1d97cce75327365de04de1da6bac972776346f67db1f610641be6288f5c890b352114bfebe8fccbf63ecd2655cba2e2c37b2bb62be61'
